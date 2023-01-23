807fbaf49502635e4749c28ccd2d863e5ff87160
mpopv <mattpopovich@outlook.com>
2023-01-23 07:44:14 -1000

    bugs

--- content.js
+++ content.js
@@ -15,7 +15,7 @@ function selectTextNodeByText(text) {
     var childNodes = element.childNodes;
     for (var j = 0; j < childNodes.length; j++) {
       var node = childNodes[j];
-      if (node.textContent.trim() === text) {
+      if (node.innerText && node.innerText.trim() === text) {
         return node;
       }
     }
@@ -26,14 +26,20 @@ function runScript() {
   try {
     const $forYou = selectTextNodeByText("For you");
     const $following = selectTextNodeByText("Following");
+
     $forYou.addEventListener("click", () => {
+      console.log("clicked for you!");
       localStorage.setItem("lastClickedFollowing", false);
     });
+
     $following.addEventListener("click", () => {
+      console.log("clicked following!");
       localStorage.setItem("lastClickedFollowing", true);
     });
+
     const lastClickedFollowing = localStorage.getItem("lastClickedFollowing");
     if (lastClickedFollowing === "true") {
+      console.log("navigating to following...");
       $following.childNodes[0].click();
     }
   } catch (e) {
