7550e321a45b469057ee939da1fa0d8a7059c5f4
mpopv <mattpopovich@outlook.com>
2023-01-23 07:06:38 -1000

    initial commit

--- content.js
+++ content.js
@@ -5,3 +5,36 @@
 // Project Repository URI: https://github.com/
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
 // License: MIT
+
+// Listen for clicks on the tabs
+
+function selectTextNodeByText(text) {
+  var allElements = document.getElementsByTagName("*");
+  for (var i = 0; i < allElements.length; i++) {
+    var element = allElements[i];
+    var childNodes = element.childNodes;
+    for (var j = 0; j < childNodes.length; j++) {
+      var node = childNodes[j];
+      if (node.nodeType === 3 && node.textContent === text) {
+        return node;
+      }
+    }
+  }
+}
+
+const $forYou = selectTextNodeByText("For you");
+const $following = selectTextNodeByText("Following");
+
+$forYou.addEventListener("click", () => {
+  localStorage.setItem("lastClickedFollowing", false);
+});
+
+$following.addEventListener("click", () => {
+  localStorage.setItem("lastClickedFollowing", true);
+});
+
+const lastClickedFollowing = localStorage.getItem("lastClickedFollowing");
+
+if (lastClickedFollowing === "true") {
+  $following.click();
+}
